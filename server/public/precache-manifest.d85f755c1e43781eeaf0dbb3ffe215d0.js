cef18cd927f1432ae582f0b112998d9d6414fca7
Alvaro Moral <aruvaro@gmail.com>
2020-05-15 10:42:11 +0200

    deploy working app

---
+++ server/public/precache-manifest.d85f755c1e43781eeaf0dbb3ffe215d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035e156cdb87878fb942ec451565478c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f19ef380378630b0351",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3ee4fffcef4c96ccb1a2",
+    "url": "/static/css/main.a55974d4.chunk.css"
+  },
+  {
+    "revision": "1f19ef380378630b0351",
+    "url": "/static/js/2.dc602619.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dc602619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee4fffcef4c96ccb1a2",
+    "url": "/static/js/main.136b5103.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
