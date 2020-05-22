ce9efec6b8fead59d6723db315cd0ff6276595ce
Alvaro Moral <aruvaro@gmail.com>
2020-05-22 11:02:01 +0200

    minor bux fixed

---
+++ server/public/img/precache-manifest.5fb814406bb043601ffaf9f1d08c189b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f397e72eed4064decdb62469816109fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40426d6735eee4e1b8c9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "857744ce7247d02da6eb",
+    "url": "/static/css/main.b22e026c.chunk.css"
+  },
+  {
+    "revision": "40426d6735eee4e1b8c9",
+    "url": "/static/js/2.541262c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.541262c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857744ce7247d02da6eb",
+    "url": "/static/js/main.60e751b6.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
